116f4122349f091e81fe3da24728f696aaa0a880
varomero <viritoco@gmail.com>
2026-09-01 09:43:05 -0400

    fix: corregir mermas y valores iniciales de cuadre en visor

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Tu Empresa - Service Worker (PWA Offline)
 // ============================================
 
-const CACHE_NAME = 'waterapp-cache-v2.6.6';
+const CACHE_NAME = 'waterapp-cache-v2.6.7';
 
 const ASSETS_TO_CACHE = [
   './',
